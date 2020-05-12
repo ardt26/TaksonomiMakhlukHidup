281010bfdaf3a73d0a812c13a3cad9fb896884f0
satya2221 <satyaghifari12@gmail.com>
2020-05-12 10:48:40 +0700

    Tampilan web

---
+++ all_data.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>All of our data</title>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #f88379;">
+	  <a class="navbar-brand" href="#"><img src="cssnya/asset/Kejaksaan_Agung_Republik_Indonesia.png" width="45" height="45"></a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav mr-auto">
+	      <li class="nav-item ">
+	      		<a class="nav-link" href="home.php">Find what is it</a>
+	      </li>
+	      <li class="nav-item active">
+	             <a class="nav-link" href="all_data.php">All our data<span class="sr-only">(current)</span></a>    
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="init_data.php">Our initial data</a>
+	      </li>
+	     </ul>
+	      <span style="color: white">Welcome</span>
+	  </div>
+	</nav>
+	<div class="container my-1">
+	<center>
+		<h1>See all of our data</h1>
+	</center>
+	<table align="center" class="table table-hover table-striped my-2 table-bordered" style="text-align: center;">
+        <thead class="table-danger">
+        <tr>
+            <th>Nomor</th>
+            <th>Hi</th>
+            <th>aku belum tahu</th>
+            <th>harus diisi</th>
+            <th>apa</th>
+        </tr>
+        <tr>
+        	<td>1</td>
+        	<td>Indonesia</td>
+        	<td>merah</td>
+        	<td>putih</td>
+        	<td>my nationality</td>
+        </tr>
+    	</thead>
+    </table>
+	</div>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #f88379;">
+  <a class="navbar-brand" href="#"><img src="cssnya/asset/Kejaksaan_Agung_Republik_Indonesia.png" width="45" height="45"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+      		<a class="nav-link" href="home.php">Find what is it<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+             <a class="nav-link" href="all_data.php">All our data</a>    
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="init_data.php">Our initial data</a>
+      </li>
+      <!-- <li class="nav-item">
+        <a class="nav-link" href="arsip.php">Arsip</a>
+      </li> -->
+      <!-- <li class="nav-item dropdown">
+        <a class="nav-link dropdown" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Notifikasi <span class="badge badge-light">3</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">
+          	<small><i>17 April 2018</i></small> <br>
+          	Response dari Kabag TU please check it
+          </a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li> -->
+      <!-- <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li> -->
+    </ul>
+      <span style="color: white">Welcome</span>  
+     <!--  <a href="proses/logout.php"><button class="btn btn-outline-dark my-2 my-sm-0 mx-2" style="color: white">Logout</button></a> -->
+  </div>
+</nav>
+
+<center>
+	<h1>Find what is it</h1>
+</center>
+</body>
+</html>
