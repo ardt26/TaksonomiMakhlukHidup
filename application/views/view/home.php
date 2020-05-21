e322d1d6b512a6b57142e7156ebc9eafbfb12d3d
ardt26 <acool90@yahoo.co.id>
2020-05-21 13:31:28 +0700

    Ubah Judul Navbar

--- application/views/view/home.php
+++ application/views/view/home.php
@@ -7,14 +7,15 @@
 </head>
 <body class="background">
 <nav class="navbar navbar-expand-lg navbar-light">
-	<a class="navbar-brand" href="#">Navbar</a>
+	<a class="navbar-brand" href="<?= base_url('TControl'); ?>">SGAR</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
+
 	<div class="collapse navbar-collapse" id="navbarNav">
 		<ul class="navbar-nav">
 			<li class="nav-item active">
-				<a class="nav-link" href="<?= base_url('TControl/index'); ?>">Home</a>
+				<a class="nav-link" href="<?= base_url('TControl'); ?>">Home</a>
 			</li>
 			<li class="nav-item">
 				<a class="nav-link" href="<?= base_url('TControl/initialData'); ?>">Initial Data</a>
